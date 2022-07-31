85f109c9ded6ddbe22d75ad659261d1bd0aa7e0e
Ayanda Mbatha <asmbatha.13@gmail.com>
2022-07-31 15:13:37 +0200

    state handler transition tests

--- bin/stateHandlers/calculate-points.test.js
+++ bin/stateHandlers/calculate-points.test.js
@@ -3,15 +3,21 @@ import { StateMachine } from '../stateMachine.js'
 
 const stateMachine = new StateMachine()
 
-test('Should correctly add up points', async () => {
-    stateMachine.state = 'calculate-points'
-    stateMachine.context.gameResults = [
-        'Test team 5, Man U 0',
-        'Barca 3, Test team 3',
-        'Test team 0, Banyana Banyana 3'
-    ]
+describe('calculate-points state handler', () => {
+    test('Should correctly add up points', async () => {
+        stateMachine.state = 'calculate-points'
+        stateMachine.context.gameResults = [
+            'Test team 5, Man U 0',
+            'Barca 3, Test team 3',
+            'Test team 0, Banyana Banyana 3'
+        ]
 
-    await handler(stateMachine)
+        await handler(stateMachine)
 
-    expect(stateMachine.context.teamPoints['Test team']).toBe(4)
+        expect(stateMachine.context.teamPoints['Test team']).toBe(4)
+    })
+
+    test('Next state should be `sort-teams`', async () => {
+        expect(stateMachine.state.name).toBe('sort-teams')
+    })
 })
--- bin/stateHandlers/output.test.js
+++ bin/stateHandlers/output.test.js
@@ -3,18 +3,20 @@ import { StateMachine } from '../stateMachine.js'
 
 const stateMachine = new StateMachine()
 
-test('Should correctly rank teams with equal points', async () => {
-    jest.spyOn(console, 'log').mockImplementation()
-    stateMachine.state = 'output'
-    stateMachine.context.league = [
-        { name: 'Tarantulas', points: 6 },
-        { name: 'Lions', points: 5 },
-        { name: 'Snakes', points: 1 },
-        { name: 'FC Awesome', points: 1 },
-        { name: 'Grouches', points: 0 }
-    ]
+describe('sort-teams state handler', () => {
+    test('Should correctly rank teams with equal points', async () => {
+        jest.spyOn(console, 'log').mockImplementation()
+        stateMachine.state = 'output'
+        stateMachine.context.league = [
+            { name: 'Tarantulas', points: 6 },
+            { name: 'Lions', points: 5 },
+            { name: 'Snakes', points: 1 },
+            { name: 'FC Awesome', points: 1 },
+            { name: 'Grouches', points: 0 }
+        ]
 
-    await handler(stateMachine)
+        await handler(stateMachine)
 
-    expect(stateMachine.context.league[2].rank).toEqual(stateMachine.context.league[3].rank)
+        expect(stateMachine.context.league[2].rank).toEqual(stateMachine.context.league[3].rank)
+    })
 })
